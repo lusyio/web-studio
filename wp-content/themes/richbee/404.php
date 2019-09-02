66e974d3c2b8db9ac9f52f8487aeabdb6013871a
Front <dimkaFPS@gmail.com>
2019-09-02 14:31:57 +0300

    страница 404

--- wp-content/themes/richbee/404.php
+++ wp-content/themes/richbee/404.php
@@ -9,29 +9,19 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-bootstrap-starter' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-bootstrap-starter' ); ?></p>
-
-					<?php
-						get_search_form();
-
-
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+<div class="container">
+    <div class="row text-center">
+        <div class="col">
+            <h1 class="new p-0">Упс, что-то пошло не так :(</h1>
+            <h2 class="sub p-0">Если вы хотите начать всё сначала, перейдите на <a href="/">Richbee</a></h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center pb-5 pt-5 mb-5">
+            <img src="/wp-content/themes/richbee/svg/undraw_page_not_found_su7k.svg" alt="">
+        </div>
+    </div>
+</div>
 
 <?php
-get_sidebar();
 get_footer();
