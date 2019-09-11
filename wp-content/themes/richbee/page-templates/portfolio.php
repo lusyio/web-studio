19321233ebb7bfcba3c9ed3b93fea8026cf730e3
Front <dimkaFPS@gmail.com>
2019-09-11 14:56:28 +0300

    выврд в портфолио всех работ

--- wp-content/themes/richbee/page-templates/portfolio.php
+++ wp-content/themes/richbee/page-templates/portfolio.php
@@ -24,7 +24,7 @@ Template Post Type: post, page, product
     <?php $portfolio_counter = 1; ?>
     <?php $image_size = array(356, 212); // выбор размера превью, выводит ближайшую по размеру в большую сторону?>
     <?php while ($catquery->have_posts()) : $catquery->the_post(); ?>
-        <?php if ($portfolio_counter == 1): ?>
+        <?php if ($portfolio_counter % 2 != 0): ?>
             <div class="row position-relative mt-custom">
                 <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.13.png" alt="">
                 <div class="col-12 order-2 order-sm-2 order-md-2 order-lg-0 order-xl-0 col-lg-6 col-xl-6">
@@ -49,7 +49,7 @@ Template Post Type: post, page, product
                     </a>
                 </div>
             </div>
-        <?php elseif ($portfolio_counter == 2): ?>
+        <?php elseif ($portfolio_counter % 2 == 0): ?>
             <div class="row mt-custom-2">
                 <div class="col-11 order-2 order-sm-1 order-md-1 offset-1 col-lg-5 order-lg-1 offset-lg-1">
                     <h2 class="new mt-0"><?php the_title(); ?></h2>
@@ -75,42 +75,6 @@ Template Post Type: post, page, product
                     </div>
                 </div>
             </div>
-        <?php elseif ($portfolio_counter == 3): ?>
-            <div class="row position-relative mt-custom-2">
-                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.13.png" alt="">
-                <div class="col-12  order-1 order-sm-2 order-md-2 order-lg-0 order-xl-0 col-lg-6 col-xl-6">
-                    <div class="bg-left-landing">
-                    </div>
-                    <div class="img-inside">
-                        <div class="position-relative">
-                            <img src="/wp-content/themes/richbee/images/macbook-top.png" style="" alt="">
-                            <img class="img-inside-mac" src="<?php the_post_thumbnail_url($image_size) ?>" alt="">
-                        </div>
-                        <img src="/wp-content/themes/richbee/images/macbook-bottom.png" alt="">
-                    </div>
-                </div>
-                <div class="col-11 order-2 order-sm-1 order-md-1 offset-1 order-lg-0 col-lg-5 offset-lg-0">
-                    <h2 class="new mt-0"><?php the_title(); ?></h2>
-                    <p class="text--secondary">
-                        <?php echo get_post_meta(get_the_ID(), 'website', true); ?>
-                    </p>
-                    <div class="description">
-                        <?php the_content(); ?>
-                    </div>
-                    <a href="<?php the_permalink() ?>">Подробнее
-                        <img src="/wp-content/themes/richbee/svg/back.svg" alt="">
-                    </a>
-                </div>
-            </div>
-        <?php elseif ($portfolio_counter == 4): ?>
-            <div class="row mt-custom-3">
-                <div class="col text-center">
-                    <button class="btn btn-input-landing text-inverse">
-                        Посмотреть другие работы
-                    </button>
-                </div>
-            </div>
-            <?php break; ?>
         <?php endif; ?>
         <?php $portfolio_counter++; ?>
     <?php endwhile; ?>
@@ -118,7 +82,5 @@ Template Post Type: post, page, product
     <?php unset($portfolio_counter); ?>
     <?php unset($image_size); ?>
 </div>
-</div>
-</div><!-- .container -->
 
 <?php get_footer(); ?>
