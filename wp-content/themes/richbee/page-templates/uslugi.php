fe1bea50cec26c65fad1d99f114c62aa27e8c3b9
Front <dimkaFPS@gmail.com>
2019-09-11 11:11:45 +0300

    страница услуги с мобилкой

--- wp-content/themes/richbee/page-templates/uslugi.php
+++ wp-content/themes/richbee/page-templates/uslugi.php
@@ -7,6 +7,190 @@ Template Post Type: post, page, product
 
 <?php get_header(); ?>
 
-Шаблон страницы услуг
+<div class="container uslugi">
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.2.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-primary text-center">
+                        <img src="/wp-content/themes/richbee/svg/browser.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Разработка сервисов
+            </h2>
+            <p>
+                от 200.000 рублей
+            </p>
+            <p>
+                Разработаем решение под вашу бизнес задачу. Автоматизация любых бизнес процессов
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.2.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-primary text-center">
+                        <img src="/wp-content/themes/richbee/svg/website.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Квиз-лендинг
+            </h2>
+            <p>
+                от 15.000 рублей
+            </p>
+            <p>
+                Увеличьте количество заявок до 400% уже через несколько дней после заказа
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.2.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-primary text-center">
+                        <img src="/wp-content/themes/richbee/svg/landing-page.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Лендинг
+            </h2>
+            <p>
+                от 50.000 рублей
+            </p>
+            <p>
+                Увеличьте количество заявок до 400% уже через несколько дней после заказа
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.2.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-primary text-center">
+                        <img src="/wp-content/themes/richbee/svg/browser.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Корпоративный сайт
+            </h2>
+            <p>
+                от 110.000 рублей
+            </p>
+            <p>
+                Разработаем современный сайт с уникальным дизайном, готовый к SEO продвижению
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.2.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-primary text-center">
+                        <img src="/wp-content/themes/richbee/svg/shopping.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Интернет магазин
+            </h2>
+            <p>
+                от 150.000 рублей
+            </p>
+            <p>
+                Получите современный интернет-магазин, готовый к интеграциям с любыми сервисами
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.1.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-warning text-center">
+                        <img src="/wp-content/themes/richbee/svg/analysis.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                SEO оптимизция
+            </h2>
+            <p>
+                от 10.000 рублей
+            </p>
+            <p>
+                Начнем выводит сайт в ТОП и увеличим трафик
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="row mt mb">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 col-lg-3">
+            <div class="card border-0">
+                <img class="bg-ellipse" src="/wp-content/themes/richbee/images/Group%203.1.png" alt="">
+                <div class="card-body">
+                    <div class="card-body-warning text-center">
+                        <img src="/wp-content/themes/richbee/svg/analysis.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-sm-11 col-md-5 col-lg-5 col-xl-5 offset-1 offset-md-0 offset-xl-1">
+            <h2 class="uslugi-h2">
+                Яндекс Директ
+            </h2>
+            <p>
+                от 25.000 рублей
+            </p>
+            <p>
+                Настроим рекламную компанию в Поиске и РСЯ. Первый месяц ведения - бесплатно. Далее от 10.000 рублей в
+                месяц
+            </p>
+        </div>
+        <div class="col-11 col-sm-11 col-md-3 col-lg-3 col-xl-2 offset-1 offset-md-0 offset-xl-1">
+            <a href="#">Заказать<img src="/wp-content/themes/richbee/svg/back.svg" alt=""></a>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
