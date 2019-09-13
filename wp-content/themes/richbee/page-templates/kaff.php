19c85170290cf12a3a7c6713b47aaa600e615905
mastekator <mastekator95@gmail.com>
2019-09-13 13:33:20 +0300

    каффы

--- wp-content/themes/richbee/page-templates/kaff.php
+++ wp-content/themes/richbee/page-templates/kaff.php
@@ -7,6 +7,44 @@ Template Post Type: post, page, product
 
 <?php get_header(); ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col mt-5">
+            <h2 class="new text-center">
+                Проект в деталях
+            </h2>
+            <div class="row">
+                <div class="col-12 col-lg-8 offset-lg-2">
+                    <p class="new">
+                        Для мастера-ювелира, который создает каждое украшение вручную, мы постарались сделать страницу
+                        товара, которая подчеркнет это.
+                    </p>
+                </div>
+            </div>
+            <div class="card rectangle mt-5">
+                <img src="/wp-content/themes/richbee/images/kaff-1.jpeg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="row mt-5">
+                <div class="col-12 col-lg-8 offset-lg-2">
+                    <p class="new">
+                        Помимо этого, мы добавили удобные фильтры в каталоге, переработали страницу заказа, добавили
+                        раздел со статьями (трафик на сайт вырос в 4 раза).
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="bg-image-rs">
+    <img src="/wp-content/themes/richbee/images/kaff-img.jpg" alt="">
+</div>
+<div class="container">
+    <div class="row">
 
-
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
