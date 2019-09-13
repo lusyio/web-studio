581b2e2c6d2257ed168e11b23f8aa44ab734a735
mastekator <mastekator95@gmail.com>
2019-09-13 10:18:46 +0300

    рубеж

--- wp-content/themes/richbee/page-templates/rubezh.php
+++ wp-content/themes/richbee/page-templates/rubezh.php
@@ -7,5 +7,44 @@ Template Post Type: post, page, product
 
 <?php get_header(); ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col mt-5">
+            <h2 class="new text-center">
+                Проект в деталях
+            </h2>
+            <div class="row">
+                <div class="col-12 col-lg-8 offset-lg-2">
+                    <p class="new">
+                        Для компании "Рубеж" потребовалось разработать небольшой сайт, на котором будет представлена
+                        продукция - щебень и СКРАП. Также мы разработали возможность добавлять новости на сайт.
+                    </p>
+                </div>
+            </div>
+            <div class="card rectangle mt-5">
+                <img src="/wp-content/themes/richbee/images/rs-cleaning.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="row mt-5">
+                <div class="col-12 col-lg-8 offset-lg-2">
+                    <p class="new">
+                        Также на сайт был добавлен раздел с вакансиями (тоже можно добавлять через административную
+                        панель), раздел контактов и реквизитов.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="bg-image-rs">
+    <img src="/wp-content/themes/richbee/images/rs-cleaning-group.jpg" alt="">
+</div>
+<div class="container">
+    <div class="row">
 
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
