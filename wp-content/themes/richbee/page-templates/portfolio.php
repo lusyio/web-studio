c70d2fd6ff61f4a0a3053718333bb4cb117c4f2c
mastekator <akmiskam@yandex.ru>
2019-10-29 12:47:13 +0300

    новое портфолио

--- wp-content/themes/richbee/page-templates/portfolio.php
+++ wp-content/themes/richbee/page-templates/portfolio.php
@@ -6,38 +6,123 @@ Template Post Type: post, page, product
 ?>
 
 <?php get_header(); ?>
-<div class="container mb-5">
-    <div class="row">
-        <div class="col text-center">
-            <h1 class="new mb-4 position-relative">Портфолио
-                <div class="singletopp">RICHBEE</div>
-            </h1>
-        </div>
-    </div>
-</div>
-<div class="container mb-5 portfolio">
-    <?php $catquery = new WP_Query('cat=19&posts_per_page=14'); // portfolio  ?>
 
-    <?php while ($catquery->have_posts()) : $catquery->the_post(); ?>
-        <div class="porfolio-block mb-5">
-            <a href="<?php the_permalink() ?>"><img class="img-protfolio"
-                                                    src="<?php the_post_thumbnail_url($image_size) ?>"
-                                                    alt="<?php the_title(); ?>"></a>
+<div class="container portfolio-tabs">
+    <div class="row">
+        <div class="col-lg-10 offset-lg-1 col-12 offset-0">
             <div class="row">
-                <div class="col-12 offset-0 col-lg-8 offset-lg-2 mt-5">
-                    <h2 class="new mt-0"><?php the_title(); ?></h2>
-                    <p class="text--secondary mb-3"><?php echo get_post_meta(get_the_ID(), 'biscat', true); ?> |
-                        <a class="text--secondary" href="<?php echo get_post_meta(get_the_ID(), 'website', true); ?>"> <?php echo get_post_meta(get_the_ID(), 'website', true); ?></a> | Сайт создан <?= the_time('d.m.Y') ?></p>
-                    <div class="description"><?php the_content(); ?></div>
-                    <a class="link-portfolio" href="<?php the_permalink() ?>">Подробнее
-                        <img src="/wp-content/themes/richbee/images/back.png" alt="">
-                    </a>
+                <div class="col-12">
+                    <ul class="nav mt-5" id="tabPortfolio" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link portfolio-tabs__link pl-0 pr-0 active" id="coopTab" data-toggle="tab"
+                               href="#coop" role="tab"
+                               aria-controls="coop" aria-selected="true">Корпоративные сайты</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link portfolio-tabs__link pl-0 pr-0" id="shopsTab" data-toggle="tab"
+                               href="#shops" role="tab" aria-controls="shops"
+                               aria-selected="true">Интернет-магазины</a>
+                        </li>
+                    </ul>
+
+                    <div class="row portfolio-tabs-tags">
+                        <div class="col-12 col-lg-6">
+                            <h3 class="portfolio-tabs-tags__header">Готовый к SEO-продвижению</h3>
+                            <p class="portfolio-tabs-tags__content">Объедините работу клиентской базы, складского учета,
+                                бухгалтерии и производства <...> в единую централизованную систему</p>
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <h3 class="portfolio-tabs-tags__header">Готовый к SEO-продвижению</h3>
+                            <p class="portfolio-tabs-tags__content">Объедините работу клиентской базы, складского учета,
+                                бухгалтерии и производства <...> в единую централизованную систему</p>
+                        </div>
+                    </div>
+
+                    <div class="row portfolio-tabs-tags">
+                        <div class="col-12 col-lg-6">
+                            <h3 class="portfolio-tabs-tags__header">Готовый к SEO-продвижению</h3>
+                            <p class="portfolio-tabs-tags__content">Объедините работу клиентской базы, складского учета,
+                                бухгалтерии и производства <...> в единую централизованную систему</p>
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <h3 class="portfolio-tabs-tags__header">Готовый к SEO-продвижению</h3>
+                            <p class="portfolio-tabs-tags__content">Объедините работу клиентской базы, складского учета,
+                                бухгалтерии и производства <...> в единую централизованную систему</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
+    </div>
 </div>
+
+<div class="container mb-5">
+
+    <div class="row">
+        <div class="col-lg-10 offset-lg-1 col-12 offset-0">
+
+            <?php $catquery = new WP_Query('cat=19&posts_per_page=14'); // portfolio  ?>
+
+            <?php while ($catquery->have_posts()) :
+                $catquery->the_post(); ?>
+
+                <div class="row portfolio-header">
+                    <div class="col-6 m-auto">
+                        <h3 class="portfolio-header__content m-0"><?php the_title(); ?></h3>
+                    </div>
+                    <div class="col-6 text-right m-auto">
+                        <a class="portfolio-header__site"
+                           href="<?php echo get_post_meta(get_the_ID(), 'website', true); ?>"><?php echo get_post_meta(get_the_ID(), 'website', true); ?></a>
+                    </div>
+                </div>
+
+                <div class="row portfolio">
+                    <div class="col">
+                        <p class="portfolio__title"><?php echo get_post_meta(get_the_ID(), 'biscat', true); ?></p>
+                        <p class="portfolio__tags">Проектировка + Разработка + Дизайн</p>
+                    </div>
+                </div>
+
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <?php
+                        global $post;
+                        $gallery = get_post_gallery_images($post);
+                        // Loop through each image in each gallery
+                        foreach ($gallery
+
+                                 as $image_url): ?>
+                            <div class="swiper-slide">
+                                <img src="<?= $image_url ?>" alt="">
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div>
+
+                <div class="row portfolio-content">
+                    <div class="col portfolio-content__text">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        </div>
+    </div>
+</div>
+<script src="/wp-content/themes/richbee/inc/assets/js/swiper.min.js"></script>
+
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+</script>
 
 <?php get_footer(); ?>
